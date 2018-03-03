30f2a661211835c5dbc5d7b85b9ceb92d7b889f1
lspengTest <37011497+lspengTest@users.noreply.github.com>
2018-03-03 15:12:14 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!wode!");
         System.out.println("goods!");
+        System.out.println("创建分支");
     }
 }
